6c7836417efe2656451db89152ae9825d8978674
buntafujikawa <bunta.fujikawa@gmail.com>
2021-08-09 15:09:39 +0900

    hookを使用しないでstateの管理を行サンプルコードの追加

---
+++ README.md
@@ -0,0 +1,6 @@
+# react-oreilly
+## references  
+[Reactベストプラクティス 第2版](https://www.oreilly.co.jp/books/9784873119380/)
+
+## support page  
+https://github.com/oreilly-japan/learning-react-2e-ja
---
+++ chapter6_state/src/AddColorForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useInput } from "./hooks";
+
+// DOM を介してデータにアクセスするコンポーネントを制御されていないコンポーネントと言う
+// export default function AddColorForm({ onNewColor = f => f }) {
+//   const txtTitle = useRef();
+//   const hexColor = useRef();
+//
+//   const submit = e => {
+//     e.preventDefault();
+//     const title = txtTitle.current.value;
+//     const color = hexColor.current.value;
+//     onNewColor(title, color);
+//     txtTitle.current.value = "";
+//     hexColor.current.value = "";
+//   }
+//
+//   return (
+//     <form onSubmit={submit}>
+//       <input ref={txtTitle} type="text" placeholder="color title..." required={}/>
+//       <input ref={hexColor} type="color" required={}/>
+//     </form>
+//   )
+// }
+
+
+export default function AddColorForm({ onNewColor = f => f }) {
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
+
+  const submit = e => {
+    e.preventDefault();
+    onNewColor(titleProps.value, colorProps.value);
+    resetTitle();
+    resetColor();
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <input
+        {...titleProps}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input {...colorProps} type="color" required />
+      <button>ADD</button>
+    </form>
+  );
+}
