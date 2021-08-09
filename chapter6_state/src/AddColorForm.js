fe6df422a63fa155cab52958f67e1422e927467d
buntafujikawa <bunta.fujikawa@gmail.com>
2021-08-09 15:10:06 +0900

    chapter6: hookを導入してstateを管理するように修正

--- chapter6_state/src/AddColorForm.js
+++ chapter6_state/src/AddColorForm.js
@@ -1,36 +1,15 @@
 import React from "react";
 import { useInput } from "./hooks";
+import { useColors } from "./ColorProvider";
 
-// DOM を介してデータにアクセスするコンポーネントを制御されていないコンポーネントと言う
-// export default function AddColorForm({ onNewColor = f => f }) {
-//   const txtTitle = useRef();
-//   const hexColor = useRef();
-//
-//   const submit = e => {
-//     e.preventDefault();
-//     const title = txtTitle.current.value;
-//     const color = hexColor.current.value;
-//     onNewColor(title, color);
-//     txtTitle.current.value = "";
-//     hexColor.current.value = "";
-//   }
-//
-//   return (
-//     <form onSubmit={submit}>
-//       <input ref={txtTitle} type="text" placeholder="color title..." required={}/>
-//       <input ref={hexColor} type="color" required={}/>
-//     </form>
-//   )
-// }
-
-
-export default function AddColorForm({ onNewColor = f => f }) {
+export default function AddColorForm() {
   const [titleProps, resetTitle] = useInput("");
   const [colorProps, resetColor] = useInput("#000000");
+  const { addColor } = useColors();
 
   const submit = e => {
     e.preventDefault();
-    onNewColor(titleProps.value, colorProps.value);
+    addColor(titleProps.value, colorProps.value);
     resetTitle();
     resetColor();
   };
--- chapter6_state/src/ColorList.js
+++ chapter6_state/src/ColorList.js
@@ -1,25 +1,23 @@
 import React from "react";
 import Color from "./Color"
+import { useColors } from "./ColorProvider";
+import { css } from "@emotion/css";
 
-export default function ColorList({
-  colors = [],
-  onRemoveColor = f => f,
-  onRateColor = f => f,
-}) {
+export default function ColorList() {
+  const { colors } = useColors();
+
   if (!colors.length) return <div>No Colors Listed. (Add a color)</div>
+
   return (
-    <div>
-      {
-        colors.map(color => (
-          <Color
-            key={color.id}
-            {...color}
-            onRemove={onRemoveColor}
-            onRate={onRateColor}
-          />
-          )
-        )
-      }
+    <div
+      className={css`
+        display: flex;
+        flex-wrap: wrap;
+      `}
+    >
+      {colors.map(color => (
+        <Color key={color.id} {...color}/>
+      ))}
     </div>
   );
 }
